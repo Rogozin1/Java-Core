4847baeedb7f91e48e2077ab8fa259fe77f14ca7
Rogozin1 <dr.test.work@gmail.com>
2022-04-18 16:29:22 +0400

    lesson3

---
+++ src/lesson3/homeWork/Apple.java
@@ -0,0 +1,10 @@
+package lesson3.homeWork;
+
+public class Apple extends Fruit {
+
+    public Apple () {
+        super(1.0f);
+    }
+
+}
+
---
+++ src/lesson3/homeWork/Orange.java
@@ -0,0 +1,9 @@
+package lesson3.homeWork;
+
+public class Orange extends Fruit {
+
+    public Orange() {
+        super(1.5f);
+    }
+
+}
